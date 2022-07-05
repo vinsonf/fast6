a4a830dbab3b04a4b2912492c7bf76b9324ef7e1
vinferno <vinsonfernandez@gmail.com>
2022-07-05 10:36:26 -0700

    feat(users): list users in template based off fauxdata

--- src/app/components/list-users/list-users.component.ts
+++ src/app/components/list-users/list-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-list-users',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListUsersComponent implements OnInit {
 
+  users$ = of([{name: 'John'}, {name: 'Jane'}]);
   constructor() { }
 
   ngOnInit(): void {
